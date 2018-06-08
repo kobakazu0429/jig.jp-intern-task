e994d3cedb3f1c26f0f4e5327532ffe3230e7a0a
kobakakzu0429 <kobakazu10@gmail.com>
2018-06-08 23:40:15 +0900

    レコード追加を関数化させた

--- src/javascripts/script.js
+++ src/javascripts/script.js
@@ -12,36 +12,25 @@ const showAll = () => {
   let table = document.getElementById('result-table');
 
   for (let i = 0; i < datas.length; i++) {
-    let tmp = datas[i];
-
-    let tr = table.insertRow(-1);
-
-    let td1 = tr.insertCell(-1);
-    let td2 = tr.insertCell(-1);
-    let td3 = tr.insertCell(-1);
-    let td4 = tr.insertCell(-1);
-    let td5 = tr.insertCell(-1);
-    let td6 = tr.insertCell(-1);
-    let td7 = tr.insertCell(-1);
-    let td8 = tr.insertCell(-1);
-    let td9 = tr.insertCell(-1);
-    let td10 = tr.insertCell(-1);
-    let td11 = tr.insertCell(-1);
-    let td12 = tr.insertCell(-1);
-    let td13 = tr.insertCell(-1);
-
-    td1.innerHTML = i + 1;
-    td2.innerHTML = tmp.event_name;
-    td3.innerHTML = tmp.category;
-    td4.innerHTML = tmp.start_date;
-    td5.innerHTML = tmp.end_date;
-    td6.innerHTML = tmp.description;
-    td7.innerHTML = tmp.schedule_description;
-    td8.innerHTML = tmp.contact;
-    td9.innerHTML = tmp.contact_phone_number;
-    td10.innerHTML = tmp.event_place;
-    td11.innerHTML = tmp.latitude;
-    td12.innerHTML = tmp.longitude;
-    td13.innerHTML = tmp.city;
+    let data = datas[i];
+    createRecord(table, data, i);
   }
+}
+
+const createRecord = (table, data, count) => {
+  let tr = table.insertRow(-1);
+
+  tr.insertCell(-1).innerHTML = count + 1;;
+  tr.insertCell(-1).innerHTML = data.event_name;;
+  tr.insertCell(-1).innerHTML = data.category;;
+  tr.insertCell(-1).innerHTML = data.start_date;;
+  tr.insertCell(-1).innerHTML = data.end_date;;
+  tr.insertCell(-1).innerHTML = data.description;;
+  tr.insertCell(-1).innerHTML = data.schedule_description;;
+  tr.insertCell(-1).innerHTML = data.contact;;
+  tr.insertCell(-1).innerHTML = data.contact_phone_number;;
+  tr.insertCell(-1).innerHTML = data.event_place;;
+  tr.insertCell(-1).innerHTML = data.latitude;;
+  tr.insertCell(-1).innerHTML = data.longitude;;
+  tr.insertCell(-1).innerHTML = data.city;;
 }
