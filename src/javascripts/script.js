c3dcc23405b9e7181a96b34a19badb940a1acdae
kobakakzu0429 <kobakazu10@gmail.com>
2018-06-08 23:41:54 +0900

    [fix] typo

--- src/javascripts/script.js
+++ src/javascripts/script.js
@@ -20,17 +20,17 @@ const showAll = () => {
 const createRecord = (table, data, count) => {
   let tr = table.insertRow(-1);
 
-  tr.insertCell(-1).innerHTML = count + 1;;
-  tr.insertCell(-1).innerHTML = data.event_name;;
-  tr.insertCell(-1).innerHTML = data.category;;
-  tr.insertCell(-1).innerHTML = data.start_date;;
-  tr.insertCell(-1).innerHTML = data.end_date;;
-  tr.insertCell(-1).innerHTML = data.description;;
-  tr.insertCell(-1).innerHTML = data.schedule_description;;
-  tr.insertCell(-1).innerHTML = data.contact;;
-  tr.insertCell(-1).innerHTML = data.contact_phone_number;;
-  tr.insertCell(-1).innerHTML = data.event_place;;
-  tr.insertCell(-1).innerHTML = data.latitude;;
-  tr.insertCell(-1).innerHTML = data.longitude;;
-  tr.insertCell(-1).innerHTML = data.city;;
+  tr.insertCell(-1).innerHTML = count + 1;
+  tr.insertCell(-1).innerHTML = data.event_name;
+  tr.insertCell(-1).innerHTML = data.category;
+  tr.insertCell(-1).innerHTML = data.start_date;
+  tr.insertCell(-1).innerHTML = data.end_date;
+  tr.insertCell(-1).innerHTML = data.description;
+  tr.insertCell(-1).innerHTML = data.schedule_description;
+  tr.insertCell(-1).innerHTML = data.contact;
+  tr.insertCell(-1).innerHTML = data.contact_phone_number;
+  tr.insertCell(-1).innerHTML = data.event_place;
+  tr.insertCell(-1).innerHTML = data.latitude;
+  tr.insertCell(-1).innerHTML = data.longitude;
+  tr.insertCell(-1).innerHTML = data.city;
 }
